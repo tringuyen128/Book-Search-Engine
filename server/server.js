c38a2abd5c4b1a6f1097dbfcbd29966a5cc0406a
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-21 10:23:21 -0700

    graphql

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const { authMiddleware } = require('./utils/auth')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
+
+server.applyMiddleware({ app })
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
