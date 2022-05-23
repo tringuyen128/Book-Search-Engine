fb2fef8aa293cb659bddbfabc89a551f4c5f8d32
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-23 12:05:17 -0700

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search-engine',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
